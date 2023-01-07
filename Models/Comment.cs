a5bb58756cbe8ab9091058035369061f8ac7d1f9
JuggernOtt81 <juggernott81@gmail.com>
2023-01-07 16:48:32 -0500

    built Comment model

---
+++ Models/Comment.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog23.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public string AuthorId { get; set; }
+        public string ModeratorId { get; set; }
+
+        [Required]
+        [StringLength(500, ErrorMessage = "The {0} of the comment must be at least {2} and at most {1} characters in length.", MinimumLength = 2)]
+        [Display(Name = "Comment")]
+        public string Body { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Created on: ")]
+        public DateTime Created { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Updated on: ")]
+        public DateTime? Updated { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Moderated on: ")]
+        public DateTime? Moderated { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Deleted on: ")]
+        public DateTime? Deleted { get; set; }
+        
+        [StringLength(500, ErrorMessage = "The {0} of the comment must be at least {2} and at most {1} characters in length.", MinimumLength = 2)]
+        [Display(Name = "Moderated Comment")]
+        public string ModeratedBody { get; set; }
+    }
+}
