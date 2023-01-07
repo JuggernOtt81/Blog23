61f47ae0b797fc18f6d607fd956fe1c97f76672f
JuggernOtt81 <juggernott81@gmail.com>
2023-01-07 15:58:14 -0500

    built blog model

--- Models/Blog.cs
+++ Models/Blog.cs
@@ -2,5 +2,14 @@
 {
     public class Blog
     {
+        public int Id { get; set; }
+        public string AuthorId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Updated { get; set; }
+        public byte[] ImageData { get; set; }
+        public string ContentType { get; set; }
+        public IFormFile Image { get; set; }
     }
 }
