9a9d91705181b158cfee6b8761065233719ed15c
JuggernOtt81 <juggernott81@gmail.com>
2023-01-07 16:19:47 -0500

    added data annotations to blog model

--- Models/Blog.cs
+++ Models/Blog.cs
@@ -1,9 +1,13 @@
-﻿namespace Blog23.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog23.Models
 {
     public class Blog
     {
         public int Id { get; set; }
         public string AuthorId { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage ="The {0} of the blog must be at least {2} and at most {1} characters in length.", MinimumLength = 2)]
         public string Name { get; set; }
         public string Description { get; set; }
         public DateTime Created { get; set; }
