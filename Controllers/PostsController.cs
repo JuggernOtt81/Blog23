e589cebf01144a73d701d77432bd82fe56523054
JuggernOtt81 <juggernott81@gmail.com>
2023-01-09 05:54:34 -0500

    carve down scaffolding for the post:edit

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -86,7 +86,7 @@ namespace Blog23.Controllers
             {
                 return NotFound();
             }
-            ViewData["BlogId"] = new SelectList(_context.Blogs, "Id", "Description", post.BlogId);
+            ViewData["BlogId"] = new SelectList(_context.Blogs, "Id", "Name", post.BlogId);
             ViewData["BlogUserId"] = new SelectList(_context.Users, "Id", "Id", post.BlogUserId);
             return View(post);
         }
@@ -96,7 +96,7 @@ namespace Blog23.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,BlogId,BlogUserId,Title,Abstract,Content,Created,Updated,ReadyStatus,Slug,ImageData,ContentType")] Post post)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,BlogId,Title,Abstract,Content,ReadyStatus,Image")] Post post)
         {
             if (id != post.Id)
             {
@@ -106,6 +106,7 @@ namespace Blog23.Controllers
             if (ModelState.IsValid)
             {
                 post.Updated= DateTime.Now.ToUniversalTime();
+                
                 try
                 {
                     _context.Update(post);
@@ -124,7 +125,7 @@ namespace Blog23.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["BlogId"] = new SelectList(_context.Blogs, "Id", "Description", post.BlogId);
+            ViewData["BlogId"] = new SelectList(_context.Blogs, "Id", "Name", post.BlogId);
             ViewData["BlogUserId"] = new SelectList(_context.Users, "Id", "Id", post.BlogUserId);
             return View(post);
         }
