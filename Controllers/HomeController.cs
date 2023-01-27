b7175abcbcb2e8247eff08ed0215a1ec9ee10ba8
JuggernOtt81 <juggernott81@gmail.com>
2023-01-27 09:17:24 -0500

    working on a contact page and form with validation and emailing function

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,12 +1,20 @@
 ﻿using Blog23.Models;
+using Blog23.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.Extensions.Logging;
+using Blog23.Services.Interfaces;
+using Blog23.Services;
+using MailKit.Security;
+using MimeKit;
+using MailKit.Net.Smtp;
 
 namespace Blog23.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IBlogEmailSender _emailSender;
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -27,6 +35,19 @@ namespace Blog23.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Contact(ContactMe model)
+        {
+            if (ModelState.IsValid)
+            {
+                //_emailSender.SendContactEmailAsync(model.Email, model.Name, model.Subject, model.Message);
+                return RedirectToAction("ContactConfirmation");
+            }
+            return View(model);
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
